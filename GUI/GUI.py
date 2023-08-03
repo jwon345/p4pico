cc9d6a2fe6bdf0b52de1eaafc75152284a36d752
jwon345 <jimmywong.024@gmail.com>
2023-08-04 03:05:02 +1200

    change init to react to OS changes

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,19 +1,35 @@
 import serial 
 import pygame
+from sys import platform
 import time
+import PositionIndex
 
-#For serial reading
+numberSensors = 16
+numberLEDs = 32
 
-#for windows machine
-#ser = Serial('COM3', 9600)
+screenWidth = 800
+screenHeight = 800
 
-#Linux Machines
-ser = serial.Serial('/dev/ttyACM0',9600) 
+connected = False
+
+
+# establishing connection based on operating system
+try:   
+    if platform == "linux" or "linux2":
+        ser = serial.Serial('/dev/ttyACM0',9600) 
+    elif platform == "win32": 
+        ser = serial.Serial('COM3',9600) 
+    else:
+        print("tf operating system?")
+    connected = True
+except:
+    connected = False
+    print("couldn't connect for some reason")
 
 #Pygame for GUI
 
 pygame.init()
-screen = pygame.display.set_mode((500,500))
+screen = pygame.display.set_mode((screenWidth,screenHeight))
 
 #Quit condition
 run = True
@@ -26,6 +42,8 @@ led = [200,250]
 #States of the LED to be varriable
 sensorStates = [True,False,False,False]
 
+LEDS = PositionIndex.calculatePositioning(numberLEDs,200,250,250)
+
 while run:
     #Exit handler
     for event in pygame.event.get():
@@ -34,11 +52,11 @@ while run:
 
             run = False
 
-  
-
     screen.fill("white")
 
   
+    for LED in LEDS:
+        pygame.draw.circle(screen, "black", (LED[1],LED[2]),10)
 
     pygame.draw.circle(screen, "black", led,10)
 
@@ -55,13 +73,13 @@ while run:
   
 
     #Reading the COM3 line. This is blocking?
+    if connected:
+        reading = ser.readline().decode()
+        #Debug print
+        print(reading)
+    else:
+        reading = [0,0,0,0]
 
-
-    reading = ser.readline().decode()
-
-    #Debug print
-
-    print(reading)
 
     for i in range(0,4):
 
