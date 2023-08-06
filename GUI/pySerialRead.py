19eb7399fbc3bbc00416d01b286b4be69adefcaa
jwon345 <jimmywong.024@gmail.com>
2023-08-06 21:01:00 +1200

    tool for simple serial reading

---
+++ GUI/pySerialRead.py
@@ -0,0 +1,40 @@
+import serial
+import time
+from sys import platform
+
+ser = serial.Serial()
+## Force Testt
+# ser = serial.Serial('/dev/pts/4',115200) 
+ser = serial.Serial('/dev/ttyUSB0',115200) 
+
+## Con for specific OS
+try:   
+    if platform == "linux" or "linux2":
+        # ser = serial.Serial('/dev/ttyUSB0',115200) 
+        # ser = serial.Serial('/dev/ttyUSB1',115200) 
+        ser = serial.Serial('/dev/ttyACM0',115200) 
+        print("connected")
+        
+    elif platform == "win32": 
+        ser = serial.Serial('COM3',9600) 
+    else:
+        print("tf operating system?")
+    connected = True
+except:
+    connected = False
+    print("couldn't connect for some reason")
+
+time.sleep(1)
+
+while True:
+    try:
+        time.sleep_ms(500)
+        print(ser.readline().decode())
+    except:
+        try:
+            print("dead")
+            ser = serial.Serial('/dev/ttyUSB0',115200) 
+            ser = serial.Serial('/dev/ttyACM0', 115200) 
+            print("connected")
+        except:
+            pass
