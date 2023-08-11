8a575297e3ad4586ca6d104ca9c23d37f33984fe
jwon345 <jimmywong.024@gmail.com>
2023-08-12 00:30:59 +1200

    fk im dumb

--- GUI/PositionIndex.py
+++ GUI/PositionIndex.py
@@ -4,16 +4,16 @@ def calculatePositioning(number, Radius, screenMidX, screenMidY):
     sensorArray = []
     for n in range(0,number):
         if n==0:
-            dX = math.sin((math.pi*2*number)) * Radius;
-            dY = math.cos((math.pi*2*number)) * Radius;
+            dX = math.sin((math.pi*2)) * Radius
+            dY = math.cos((math.pi*2)) * Radius
             sensorArray.append([
                 n, 
-                (int)(screenMidX+dX+0.5),
+                (int)(screenMidX+dX+0.5), #0.5 for integer round down
                 (int)(screenMidY-dY+0.5),
             ])
             continue
-        dX = math.sin((math.pi*number)/n) * Radius;
-        dY = math.cos((math.pi*number)/n) * Radius;
+        dX = math.sin((2*math.pi) * (n/number)) * Radius
+        dY = math.cos((2*math.pi) * (n/number)) * Radius
         sensorArray.append([
             n, 
             (int)(screenMidX+dX+0.5),
@@ -22,5 +22,6 @@ def calculatePositioning(number, Radius, screenMidX, screenMidY):
     
     return sensorArray
 
-
-print(calculatePositioning(10,10,50,50))
+# testing
+print(calculatePositioning(4,50,50,50))
+input() # stop close
